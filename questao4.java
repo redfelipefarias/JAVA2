fc161a87ee99b1a942b32d9b3b1d45e9dffb0398
redfelipefarias <142460742+redfelipefarias@users.noreply.github.com>
2024-08-22 21:16:44 -0300

    Add files via upload

---
+++ questao4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class questao4 {
+    public static void main(String[] args) {
+        Scanner l = new Scanner(System.in);
+
+        System.out.println("DIGITE UM VALOR INTEIRO PARA A LETRA (A): ");
+        int a = l.nextInt();
+        System.out.println("DIGITE UM VALOR INTEIRO PARA A LETRA (B): ");
+        int b = l.nextInt():
+
+        if (a == b){
+
+        }
+
+    }
+}
