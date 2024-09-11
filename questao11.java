156ddc9246fcec7e515f526ebe18e7aaf226b7f6
redfelipefarias <142460742+redfelipefarias@users.noreply.github.com>
2024-09-11 19:22:07 -0300

    Update questao11.java

--- questao11.java
+++ questao11.java
@@ -1,7 +1,4 @@
 import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class q11 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
